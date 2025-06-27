653ac4fb6edc5d48d8e8bf9719fb4659358c6d6e
Maria-Nascimento06 <nascimentoeduarda182@gmail.com>
2025-06-27 16:09:41 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,165 @@
+/*alert('boas vindas ao jogo do número secreto')
+let numeroSecreto = (Math.random() * 100 + 1);
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
+
+while (chute != numeroSecreto) {
+    chute = prompt(`Escolha um numero entre 1 e 100`);
+
+if (chute == numeroSecreto) {
+    
+} else {
+    if (chute > numeroSecreto) {
+        alert(`o número secreto é menor que ${chute}`);
+    } else {
+        alert(`o número secreto é maior que ${chute}`);
+    }
+    tentativas++;
+} 
+} 
+
+let palavraTentativa = tentativas > 1 ? 'tentativas': 'tentativa';
+    alert(`Isso ai! voce descobriu o numero secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}` );
+
+
+if (tentativas >1){
+    alert(`Isso ai! voce descobriu o numero secreto ${numeroSecreto} com ${tentativas} tentativas` );
+} else {14
+    alert(`Isso ai! voce descobriu o numero secreto ${numeroSecreto} com ${tentativas} tentativa` );
+}
+
+
+
+//let palavraPessoa = "";
+//if(quantidadedePessoa == 1){
+    palavraPessoa = "pessoa";
+//} else{
+    //palavraPessoa = "pessoas"
+//}
+
+ //let palavraPessoa = quantidadedePessoas > 1 ? 'pessoas': 'pessoa';
+
+
+
+/*alert('boas vindas ao nosso site');
+let nome = 'lua'
+var idade = 25
+var numeroDeVendas = 50
+var saldoDisponivel = 1000
+alet ('Erro! Preencha todos os campos')
+var mensagemDeErro = 'Erro! Preencha todos os campos!'
+var nome = prompt ('Digite a sua idade')
+
+alert('Qual o dia da semana?')
+let diaSemana = prompt('Digite o dia da semana')
+if (chute == domingo || sabado ){
+    alert('Bom final de semana');
+} else {
+    alert('Boa semana')
+}
+alert('Qual é o mes?')
+let mes = prompt('Digite o mes');
+if (mes === 'dezembro' || mes === 'janeiro' || mes === 'fevereiro' ){
+    alert('Estamos no verão!');
+} else {
+    alert('Não é verão')
+} 
+alert('Qual é o horário (formato 24h)?');
+let horario = prompt('Digite o horário');
+if (horario <12){
+    alert('Bom dia')
+}
+
+alert('vamos calcular a sua media');
+let media1 = parseFloat(prompt('Digite sua primeira nota'));
+let media2 = parseFloat(prompt('Digite sua segunda nota'))
+let media3 = parseFloat(prompt('Digite sua terceira nota'));
+let mediaSoma = (media1 + media2 + media3) /3 ;
+alert(mediaSoma);
+
+if (mediaSoma =>5){
+    alert('Voce aprovou');
+} else {
+    alert('Voce reprovou');
+}
+
+alert('vamos contar');
+let contador = 0;
+while (contador <= 10) {
+    alert(contador);
+    contador++;
+}*/
+
+
+/*let qtdNumero = prompt('Digite a quantidade de números')
+let soma = 0;
+let contador = qtdNumero;
+while(contador > qtdNumeros){
+let numero = parseInt(prompt(Digite o número:'));
+soma += numero;
+}
+contador +1
+let media = soma/ qtdNumero;
+console.log(media);
+alert('boas vindas')
+alert('Ola Maria')
+let = prompt('Qual a linguagem de programação que voce mais gosta?')
+
+
+var valor1 = 2
+var valor2 = 4
+var resultado = (valor1 - valor2);
+console.log(`a soma de ${valor1} e ${valor2} igual a ${resultado}` );
+let idade2 = idade > 18 ? 'voce é maior de idade': 'voce é menor de idade';
+alert('voce é menor')*/
+
+//7
+let idade = prompt("Digite sua idade:");
+ if (idade >= 18) {
+    alert ("Você e maior de idade")
+ } else {
+    alert("você e menor de idade")
+ } 
+
+ //8 não terminado 
+let numero = prompt("Digite um numero:");
+if (numero >= 10) {
+    alert ("seu valor é positivo")
+ } else {
+    alert("seu valor é negativo")
+ } 
+
+
+//falta 9 - use um loop while para imprimir os numeros de 1 a 10 no console
+
+//numero = 1 
+//while
+
+//10
+let nota = prompt("Digite a nota:");
+ if (nota >= 7) {
+    alert("aprovado")
+ } else {
+    alert("reprovado")
+ } 
+
+
+//11
+ let numero1 = (Math.random() * 50 + 1);
+console.log(numero1);
+
+//12
+let numero2 = (Math.random() * 10 + 1);
+console.log(numero2);
+
+//13
+let numero3 = (Math.random() * 1000 + 1);
+console.log(numero3);
+
+
+
+
+
+
+ 
