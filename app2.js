bd5b9a6306003a766b4c6310364ada89a253526b
Maria-Nascimento06 <nascimentoeduarda182@gmail.com>
2025-07-03 14:07:45 -0300

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,84 @@
+//1 terminado?
+alert('boas vindas');
+
+//2 terminado?
+let = prompt('Qual a linguagem de programação que voce mais gosta?')
+
+//3 falta no resultado
+let a = 5;
+let b = 3;
+let soma = a + b;
+console.log('a soma de a+b = soma');
+//resultado:8
+let subtracao = a - b;
+console.log("A subtração é:")
+ // Resultado:2
+
+
+//4 terminado?
+alert('digite sua idade');
+//var = idade
+if (idade >= 18) {
+    alert('Você é maior de idade');
+  } 
+  
+  //else {
+   // alert('Você é menor de idade');
+ //}
+
+ //5 else if = para verificar múltiplas(mais de uma) condições em sequencia/ pesquisa
+ if (numero > 0) {
+    alert('O número é positivo');
+  } else if (numero < 0) {
+    alert('O número é negativo');
+  } else {
+    alert('O número é zero');
+  }
+
+  //6 falta algo
+  let numero = 1;
+while (contador <= 10) {
+    console.log("Número:", contador);
+    contador++;
+}
+// Incrementa o valor de contador
+
+//7 terminado?
+let nota = prompt("Digite a nota:");
+ if (nota >= 5) {
+    alert("aprovado")
+ } else {
+    alert("reprovado")
+ } 
+
+
+//8 terminado?
+let numero1 = (Math.random *10 + 1);
+console.log(numero1)
+
+let numero2 = (Math.random *1000 + 1);
+console.log(numero2)
+
+let numero3 = (Math.random *30000 + 1);
+console.log(numero3) 
+
+
+//exemplo de uso da atividade 
+let nome = "Seu Nome";
+console.log("Olá, " + nome + "!");
+alert("Olá, " + nome + "!");
+
+let linguagem1 = prompt("Qual a linguagem de programação que você mais gosta?");
+console.log("Você gosta de: " + linguagem1);
+
+//exemplo de uso da atividade + modificado
+let sobrenome = "Seu sobrenome";
+console.log("Olá, " + sobrenome + "(:");
+alert("Olá, " + sobrenome + "(:");
+
+let linguagem2 = prompt("Qual a linguagem de programação que você menos gosta?");
+console.log("Você gosta de: " + linguagem2);
+
+
+
+
